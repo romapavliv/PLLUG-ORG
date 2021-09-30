8eeff22cfab030e4d4892ad7259ffd44f8648d45
Nazar <76834306+Nazar-Lysak@users.noreply.github.com>
2021-09-30 18:04:58 +0300

    Update and rename Pllug-tasks.js to Pllug-task-1.js

---
+++ Pllug-task-1.js
@@ -0,0 +1,11 @@
+function strCase(str) {
+  let newArr = "";
+  for (let i = 0; i < str.length; i++) {
+    str[i] !== str[i].toUpperCase()
+      ? newArr = newArr + str[i].toUpperCase()
+      : newArr = newArr + str[i].toLowerCase();
+  }
+  return newArr;
+}
+
+console.log(strCase("iT`S jAVA sCRIPT =)"));
