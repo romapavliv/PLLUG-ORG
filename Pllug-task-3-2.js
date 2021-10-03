f295523fe557ae17446f4e5acae3b66648ed3fdb
Nazar <76834306+Nazar-Lysak@users.noreply.github.com>
2021-10-04 00:49:56 +0300

    Update Pllug-task-3-2.js

--- Pllug-task-3-2.js
+++ Pllug-task-3-2.js
@@ -20,30 +20,30 @@ window.addEventListener("unload", function() {
 // Скрипт який виводить повертає назву браузера та назву операційної системи через дефіс "-" як одну строку.
 
 function find_br() {
-  let sBrowser,
-    sUsrAg = navigator.userAgent;
-  if (sUsrAg.indexOf("Firefox") > -1) sBrowser = "Mozilla Firefox";
-  else if (sUsrAg.indexOf("Opera") > -1) sBrowser = "Opera";
-  else if (sUsrAg.indexOf("Trident") > -1) sBrowser = "Microsoft Internet Explorer";
-  else if (sUsrAg.indexOf("Edge") > -1) sBrowser = "Microsoft Edge";
-  else if (sUsrAg.indexOf("Chrome") > -1) sBrowser = "Google Chrome";
-  else if (sUsrAg.indexOf("Safari") > -1) sBrowser = "Apple Safari";
-  else sBrowser = "unknown";
-  return sBrowser;
+    let sBrowser,
+        sUsrAg = navigator.userAgent;
+    if (sUsrAg.indexOf("Firefox") > -1) sBrowser = "Mozilla Firefox";
+    else if (sUsrAg.indexOf("Opera") > -1) sBrowser = "Opera";
+    else if (sUsrAg.indexOf("Trident") > -1) sBrowser = "Microsoft Internet Explorer";
+    else if (sUsrAg.indexOf("Edge") > -1) sBrowser = "Microsoft Edge";
+    else if (sUsrAg.indexOf("Chrome") > -1) sBrowser = "Google Chrome";
+    else if (sUsrAg.indexOf("Safari") > -1) sBrowser = "Apple Safari";
+    else sBrowser = "unknown";
+    return sBrowser;
 }
 
 function find_os() {
-  let OSVer = "";
-  if (navigator.userAgent.indexOf("Mac OS X 10.4") != -1) OSVer = "MacOS Tiger";
-  if (navigator.userAgent.indexOf("Mac OS X 10.5") != -1) OSVer = "MacOS Leopard";
-  if (navigator.userAgent.indexOf("Mac OS X 10.6") != -1) OSVer = "MacOS Snow Leopard";
-  if (navigator.userAgent.indexOf("NT 5.1") != -1) OSVer = "Windows XP";
-  if (navigator.userAgent.indexOf("NT 6.0") != -1) OSVer = "Windows Vista";
-  if (navigator.userAgent.indexOf("NT 6.1") != -1) OSVer = "Windows 7";
-  if (navigator.userAgent.indexOf("Linux") != -1) OSVer = "Linux";
-  if (navigator.userAgent.indexOf("X11") != -1) OSVer = "UNIX";
+    let OSVer = "";
+    if (navigator.userAgent.indexOf("Mac OS X 10.4") != -1) OSVer = "MacOS Tiger";
+    if (navigator.userAgent.indexOf("Mac OS X 10.5") != -1) OSVer = "MacOS Leopard";
+    if (navigator.userAgent.indexOf("Mac OS X 10.6") != -1) OSVer = "MacOS Snow Leopard";
+    if (navigator.userAgent.indexOf("NT 5.1") != -1) OSVer = "Windows XP";
+    if (navigator.userAgent.indexOf("NT 6.0") != -1) OSVer = "Windows Vista";
+    if (navigator.userAgent.indexOf("NT 6.1") != -1) OSVer = "Windows 7";
+    if (navigator.userAgent.indexOf("Linux") != -1) OSVer = "Linux";
+    if (navigator.userAgent.indexOf("X11") != -1) OSVer = "UNIX";
 
-  return OSVer;
+    return OSVer;
 }
 
 console.log(`${find_br()} - ${find_os()}`);
