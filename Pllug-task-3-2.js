75d931edb3610a4fa6c2115cce23c066476464ab
Nazar <76834306+Nazar-Lysak@users.noreply.github.com>
2021-10-03 21:45:04 +0300

    Create Pllug-task-3-2.js

---
+++ Pllug-task-3-2.js
@@ -0,0 +1,46 @@
+// Скрипт який виводить в консоль дату і час коли DOM побудований, але css, зображення ще не завантажились.
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(new Date(),"DOMContentLoaded");
+})
+
+// Скрипт який виводить в консоль дату і час коли DOM побудований і css, зображення вже завантажились.
+
+window.onload = () => {
+    console.log(new Date(), "onload");
+}
+
+// Скрипт який виводить в консоль дату і час коли юзер натиснув на закрити вкладку або перезавантажити сторінку.
+
+window.addEventListener("unload", function() {
+    console.log(new Date(), "unload - true");
+    localStorage.setItem("unload", "true");
+  })
+
+// Скрипт який виводить повертає назву браузера та назву операційної системи через дефіс "-" як одну строку.
+
+// Скрипт який змушує браузер показувати сповіщення, коли ви намагаєтесь перезавантажити сторінку, видалити вкладку, чи вимкнути браузер.
+
+window.onbeforeunload = function () {
+    console.log("Ви впевнені, що бажаєте оновити/закрити сторінку?");
+    return "";
+}
+
+// Скрипт який виводить в консоль скільки секунд юзер перебував на сторінці, після того як юзер натиснув на закрити вкладку або перезавантажити сторінку.
+
+let startTime;
+let endTime;
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Time Tracker was started");
+    startTime = new Date();
+    for (let i = 0; i < 1000; i++) {
+        Math.sqrt(i);
+    }
+});
+
+window.onbeforeunload = function () {
+    endTime = new Date();
+    console.log('Час перебування на сторінці становив: ' + (endTime.getTime() - startTime.getTime()) + ' мсек');
+    return "";
+}
