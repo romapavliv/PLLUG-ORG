b27a31d3aa9dc36aab282ef4e0f65e4d6f19098e
Nazar <76834306+Nazar-Lysak@users.noreply.github.com>
2021-10-16 00:41:03 +0300

    Update Pllug-task-7.js

--- Pllug-task-7.js
+++ Pllug-task-7.js
@@ -1,1 +1,22 @@
 
+// Написати функцію, яка приймає об’єкт у якості аргументу та повертає Map  з тими самими даними. Тобто просто перетворити об’єкт у Map.
+const object = {1: 'a', 2: 'b', 3: 'c'};
+
+const toMap = (obj) => {
+    return new Map(Object.entries(obj));
+}
+
+const map = toMap(object);
+
+// Написати функцію, яка приймає Map у якості аргументу та повертає об’єкт з тими самими даними. Тобто просто перетворити Map у  об’єкт.
+
+const toObj = (map) => {
+    return Object.fromEntries(map);
+}
+
+const obj = toObj(map);
+
+console.group();
+console.log(obj);
+console.log(map);
+console.groupEnd();
